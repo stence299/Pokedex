32183607d672a5cbeee54191d68e7316a517257b
stence299 <stence299@gmail.com>
2017-11-04 23:17:33 -0500

    added a bit of styling on the pokemon page

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const Filter = require('react-filter');
 const massive = require('massive');
 const connectionString = require("./config").dbConnection
 const dbController = require("./dbController.js")
